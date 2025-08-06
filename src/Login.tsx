d443bc5c0cf949ebdd6343c62fd2c57b0f39f45a
susanladyyy <susanrumokoy02@gmail.com>
2025-08-06 20:26:38 +0700

    first commit mockup oh print

---
+++ src/Login.tsx
@@ -0,0 +1,131 @@
+import ohPrintLogo from '/logo.jpg'
+import './Login.css'
+import { useCallback } from "react";
+import type { Container, Engine } from "tsparticles-engine";
+import Particles from "react-particles";
+import { loadSlim } from "tsparticles-slim";
+
+function Login() {
+  const particlesInit = useCallback(async (engine: Engine) => {
+        console.log(engine);
+
+        // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        //await loadFull(engine);
+        await loadSlim(engine);
+    }, []);
+
+  const particlesLoaded = useCallback(async (container: Container | undefined) => {
+      await console.log(container);
+  }, []);
+  
+  return (
+    <>
+      <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+                background: {
+                    color: {
+                        value: "#f8951e",
+                    },
+                },
+                fpsLimit: 80,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#ddddddff",
+                    },
+                    links: {
+                        color: "#ffffff",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 6,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 5 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
+
+        <div className='container'>
+          <div>
+            <img src={ohPrintLogo} className="logo" alt="OH!Print logo" />
+          </div>
+          <div className="card">
+            <div className="email-input">
+              <div className="email-label">
+                <label htmlFor="">Email</label>
+              </div>
+              <div className="">
+                <input type="text" name="" id="" />
+              </div>
+            </div>
+            <div className="pass-input">
+              <div className="pass-label">
+                <label htmlFor="">Password</label>
+              </div>
+              <div className="">
+                <input type="password" name="" id="" />
+              </div>
+            </div>
+          </div>
+          <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
+          </p>
+        </div>
+    </>
+    
+  )
+}
+
+export default Login
